da38c3976ee18f27f6a11977f3dfa50fd7583f24
HENRY YU <jay@sparkon.org>
2012-12-17 18:27:25 -0800

    Cleaned up sprites.js

--- popsql/models/sprites.js
+++ popsql/models/sprites.js
@@ -1,13 +1,11 @@
 
 module.exports = { 
-	handle: function(req1, req2, callback) {
-		data = req1
-	,	users_ip = req2;
-		for (var i = 0; i < data.length; i++) {
-			if (data[i].spriteID != '/images/host.png' && data[i].user_ip != users_ip) {
-				data[i].spriteID = '/images/guest_b.png';
+	handle: function(post, users_ip, callback) {
+		for (var i = 0; i < post.length; i++) {
+			if (post[i].spriteID != '/images/host.png' && post[i].user_ip != users_ip) {
+				post[i].spriteID = '/images/guest_b.png';
 			}
 		}
-		return callback(data);
+		return callback(post);
 	}
 };
